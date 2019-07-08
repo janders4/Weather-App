8ca12f7f294bcd7bd26c8bdb879c165120b329f3
janders4 <johnanderson198689@gmail.com>
2019-07-08 12:45:42 +0100

    more basics added. css needs linking

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Weather = props => {
-  return <p>Weather goes here</p>;
+  return (
+    <div>
+      <p>City goes here</p>
+      <img />
+      <h2>Temp</h2>
+      <h3>Description</h3>
+    </div>
+  );
 };
 
 //weather.propTypes = {};
