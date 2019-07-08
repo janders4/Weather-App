7086fdb1e60dbe41ddc0ca18260bce08c7dc80c3
janders4 <johnanderson198689@gmail.com>
2019-07-08 12:20:09 +0100

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const header = props => {
+  return <h1>What's the Weather like?</h1>;
+};
+
+header.propTypes = {};
+
+export default header;
